7d5257fb2bc3a2c18d4eb5c80bbeb7356d5f0fe8
Andrew Leifer <andrew.leifer@gmail.com>
2012-06-28 15:26:35 -0400

    problem 3

--- modeling_lab/assignment/prob3.m
+++ modeling_lab/assignment/prob3.m
@@ -1,1 +1,8 @@
-load('../data/cell1_test_spks.mat')
+%Load in spike times for cell 1 of training
+load('../data/cell1_train_spks.mat')
+
+
+bincenters=0:.01:2;
+counts=histc(diff(s),bincenters);;
+
+figure; plot(bincenters,counts)
