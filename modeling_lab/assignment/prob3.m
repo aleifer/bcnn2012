ab2a180e8748a6949a05c3e84b369e6032aa53ca
Andrew Leifer <andrew.leifer@gmail.com>
2012-06-28 15:48:09 -0400

    finised prob3

--- modeling_lab/assignment/prob3.m
+++ modeling_lab/assignment/prob3.m
@@ -1,8 +1,28 @@
 %Load in spike times for cell 1 of training
 load('../data/cell1_train_spks.mat')
 
+%number of spikes
+N=length(s);
 
-bincenters=0:.01:2;
-counts=histc(diff(s),bincenters);;
+%lambda is the mean spike rate
+lambda=N/s(end);
 
-figure; plot(bincenters,counts)
+
+
+binwidth=.001;
+bincenters=0:binwidth:2;
+counts=histc(diff(s),bincenters);
+
+%Probability of an interspike interval is the number of counts per bin
+%divided by N
+pISI=counts./N; 
+
+
+figure; plot(bincenters,pISI)
+xlabel('Time of Interval (s)')
+ylabel('Probability of Spike')
+
+hold on;
+plot(bincenters,lambda.*exp(-lambda*bincenters).*binwidth,'r')
+title('PDF of Inter Spike Interval Cell 1 Train Spikes')
+xlim([0 .4])
