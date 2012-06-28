937438ccd4bdf2c6a916671253b05d50003007b4
Andrew Leifer <andrew.leifer@gmail.com>
2012-06-28 17:37:49 -0400

    totally redid prob3. correct now

--- modeling_lab/assignment/prob3.m
+++ modeling_lab/assignment/prob3.m
@@ -1,30 +1,32 @@
+%Andrew Leifer
+%leifer@princeton.edu
+%With Vivek Venkatachalam
 %Load in spike times for cell 1 of training
-load('../data/cell1_train_spks.mat')
-
-%number of spikes
-N=length(s);
-
-%lambda is the mean spike rate
-lambda=N/s(end);
+load('../data/cell1_test_spks.mat')
 
 
-
-binwidth=.001;
-bincenters=0:binwidth:2;
-counts=histc(diff(s),bincenters);
-
-%Probability of an interspike interval is the number of counts per bin
-%divided by N
-pISI=counts./N; 
+%Chop the trials at the reset from 30s to 0s
+chopPoints=find(diff(s)<0);
+chopPoints=[1 chopPoints length(s)];
 
 
-figure; plot(bincenters,pISI)
-xlabel('Time of Interval (s)')
-ylabel('Probability of Spike')
+for k=1:length(chopPoints)-1
+    spikeEvents{k}= s(chopPoints(k):chopPoints(k+1));
+end
 
-hold on;
-plot(bincenters,lambda.*exp(-lambda*bincenters).*binwidth,'r')
-title('PDF of Inter Spike Interval Cell 1 Train Spikes')
-xlim([0 .4])
-text(.2,.05, ['\lambda=' num2str(mean(diff(s))) ', var=' num2str(var(diff(s)))]);
-    
+%Now we need to bin the individual trials
+binSize=.005;
+
+bins = 0:binSize:30; %10 ms bins
+spikesPerTime = zeros(length(spikeEvents),length(bins));
+
+for k=1:length(spikeEvents)
+    spikesPerTime(k,:)= histc(spikeEvents{k},bins);
+end
+
+meanSpkRate=mean(spikesPerTime);
+varSpkRate=var(spikesPerTime);
+figure;
+plot(meanSpkRate,varSpkRate,'o',[0 3],[0 3]);
+xlabel(['Mean Spike Rate (spikes/ ' num2str(binSize*1000) 'ms)'])
+ylabel(['Variance (spikes /' num2str(binSize*1000) 'ms)'])
